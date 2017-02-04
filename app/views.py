c66d4e51e3b103e309f4feca5b0eef87eb4297ec
Priya@gmail.com <priyankasreddy95@gmail.com>
2017-02-04 18:01:35 +0530

    added view and models

--- app/views.py
+++ app/views.py
@@ -1,10 +1,55 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404,redirect
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
 # Create your views here.
 
 def post_list(request):
     posts = Post.objects.filter(publish_date=timezone.now()).order_by('publish_date')
     return  render(request, 'app/post_list.html', {'posts': posts})
+
+def post_new(request):
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('post_detail', pk = post.pk)
+        else:
+            form = PostForm()
+            return render(request, 'blog/post_edit.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST" :
+        form = PostForm(request.POST, instance = post)
+        if form.is_valid():
+            post = form.save(commit = False)
+            post.author = request.user
+            post.save()
+            return redirect('post_details', pk=post.pk)
+        else:
+            form = PostForm(instance = post)
+            return render(request, 'blog/post_edit.html', {'form': form})
+
+
+def post_draft_list(request):
+    posts = Post.objects.filter(published_date__isnull = True).order_by('created_date')
+    return render(request, 'blog/post_draft_list.html', {'post' : posts})
+
+def post_publish(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.publish()
+    return  redirect('post_details', pk=pk)
+
+def publish(self):
+    self.publish_date = timezone.now()
+    self.save()
+
+def post_remove(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    post.delete()
+    return redirect('post_list')
 
